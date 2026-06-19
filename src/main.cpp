79f85366c9f4cee428d188b5c12f45120151503a
Várnai Gellért <gellert.varnai@webandmore.hu>
2026-06-19 03:30:48 +0200

    wifi status serial cmd

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <WifiConfig.hpp>
+#include <SerialHandler.hpp>
 #include <arduino-timer.h>
 #include "secrets.h"
 
 #define PIN_LED 16
+
+void serialCb(String);
 
 bool debug = true;
 WifiConfig wifiConfig(WIFI_SSID, WIFI_PASSWORD, "ESP32S2 Tester Hub", "s2-tester", AUTH_USER, AUTH_PASS, true, true, debug);
@@ -29,8 +32,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  handleSerial(debug, serialCb);
   wifiConfig.loop();
   timer.tick<void>();
   delay(1);
 }
 
+void serialCb(String buffer) {
+  if (buffer == "s") {
+    Serial.println("Wifi Status: " + String(WiFi.status()));
+  }
+}
